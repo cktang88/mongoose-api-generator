54d77d933fd25ed2fb1c7c8fa35609c166518e7b
cktang88 <arctangent20@yahoo.com>
2020-06-21 13:04:07 -0500

    Fix server import

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRouter = require("./framework/auth/authRouter");
-const apiRouter = require("./framework/core/api");
+const apiRouter = require("./framework/core/api.js");
 const pino = require("pino-http")();
 
 const app = express();
